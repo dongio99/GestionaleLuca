d5d1c410d94fcf6a19a374a5d11f2905980f7467
dongio99 <dongiovanni.gabriele@gmail.com>
2024-02-12 23:45:24 +0100

    add MagazzinoView

--- gestionale/Magazzino/views.py
+++ gestionale/Magazzino/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Magazzino
 
-# Create your views here.
+class MagazzinoView(View):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        prodotti = Magazzino.objects.filter(quantita__gt=0)
+        context = {"giacenza": prodotti}
+        return render(request, self.template_name, context)
