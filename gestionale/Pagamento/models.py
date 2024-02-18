4b34fd2ca5a0f01e0aa74186b3b1bdc86ff848c5
dongio99 <dongiovanni.gabriele@gmail.com>
2024-02-18 18:07:49 +0100

    Rm app Pagamento. Class Pagamento ora in app Fornitore

--- gestionale/Pagamento/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Pagamento(models.Model):
-    descrizione = models.CharField(max_length=255)
-
-    @staticmethod
-    def get_mod_pagamento():
-        return [(p.id, p.descrizione) for p in Pagamento.objects.all()]
