2195e285dd017178d15a3e08794a1780078d6fbc
dongio99 <dongiovanni.gabriele@gmail.com>
2024-02-18 18:06:12 +0100

    MagazzinoView TO ADD tale search

--- gestionale/Magazzino/views.py
+++ gestionale/Magazzino/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from .models import Magazzino
 
+
 class MagazzinoView(View):
-    template_name = 'index.html'
+    template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
         prodotti = Magazzino.objects.filter(quantita__gt=0)
