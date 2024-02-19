7d4d180835b1a80115a8aa49f078dbacd68c84b5
dongio99 <dongiovanni.gabriele@gmail.com>
2024-02-20 00:15:38 +0100

    add def value

--- gestionale/Negozio/models.py
+++ gestionale/Negozio/models.py
@@ -6,19 +6,19 @@ from Iva.models import Iva
 
 class Negozio(models.Model):
     prodotto = models.ForeignKey(Prodotto, on_delete=models.CASCADE)
-    quantita = models.IntegerField()
-    imponibile_non_scontato = models.DecimalField(max_digits=10, decimal_places=2)
-    iva = models.DecimalField(choices=Iva.getChoices(), max_digits=5, decimal_places=2)
-    prezzo_finale_non_scontato = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    quantita = models.IntegerField(default=0.00)
+    imponibile_non_scontato = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    iva = models.DecimalField(choices=Iva.getChoices(), max_digits=5, decimal_places=2, default=22.00)
+    prezzo_finale_non_scontato = models.DecimalField(max_digits=10, decimal_places=2,  default=0.00, editable=False)
     sconto = models.DecimalField(
         max_digits=5,
         decimal_places=2,
         default=0.00,
         validators=[MinValueValidator(0), MaxValueValidator(100)],
     )
-    prezzo_finale_scontato = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    prezzo_finale_scontato = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, editable=False)
 
-    def save(self) -> None:
-        self.prezzo_finale = self.imponibile * (1 + self.iva / 100)
-        self.prezzo_finale_scontato = self.imponibile * (1 + self.iva / 100) * (1 - self.sconto / 100)
+    def save(self, *args, **kwarg) -> None:
+        self.prezzo_finale = self.imponibile_non_scontato * (1 + self.iva / 100)
+        self.prezzo_finale_scontato = self.imponibile_non_scontato * (1 + self.iva / 100) * (1 - self.sconto / 100)
         return super().save()
