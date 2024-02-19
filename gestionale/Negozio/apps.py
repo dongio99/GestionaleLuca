4638e915f5ff2278b1e4128a3c8c6e3862d61098
dongio99 <dongiovanni.gabriele@gmail.com>
2024-02-20 00:15:22 +0100

    creazione negozio a creazione prodotto

--- gestionale/Negozio/apps.py
+++ gestionale/Negozio/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class NegozioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "Negozio"
+
+    def ready(self):
+        import Negozio.signals
---
+++ gestionale/Negozio/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.apps import apps
+from .models import Negozio
+
+
+@receiver(post_save, sender=apps.get_model("Prodotto", "Prodotto"))
+def initialize_giacenza(sender, instance, created, **kwargs):
+    if created:
+        Negozio.objects.create(prodotto=instance)
